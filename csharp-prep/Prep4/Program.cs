beaf4589ce1986b082239f49830ffa80d89087d6
romelp84 <romelp84@hotmail.com>
2024-03-02 15:41:46 -0500

    Prep4 Completed

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,63 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int input;
+        do
+        {
+            Console.Write("Enter number: ");
+            input = Convert.ToInt32(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+
+        } while (input != 0);
+
+        // Core Requirements
+        int sum = CalculateSum(numbers);
+        double average = CalculateAverage(numbers);
+        int maxNumber = FindMaxNumber(numbers);
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
+    }
+
+    static int CalculateSum(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+
+    static double CalculateAverage(List<int> numbers)
+    {
+        int sum = CalculateSum(numbers);
+        return (double)sum / numbers.Count;
+    }
+
+    static int FindMaxNumber(List<int> numbers)
+    {
+        int max = int.MinValue;
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        return max;
     }
 }
