33b3c6dd05a832e489f301b832e7c17132f92dad
romelp84 <romelp84@hotmail.com>
2024-04-09 20:45:26 -0500

    Foundation 3,4 completed

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -1,9 +1,124 @@
 using System;
 
+class Event
+{
+    public string EventTitle { get; set; }
+    public string Description { get; set; }
+    public DateTime Date { get; set; }
+    public TimeSpan Time { get; set; }
+    public Address Address { get; set; }
+
+    public Event(string title, string desc, DateTime date, TimeSpan time, Address address)
+    {
+        EventTitle = title;
+        Description = desc;
+        Date = date;
+        Time = time;
+        Address = address;
+    }
+
+    public virtual string GenerateStandardMessage()
+    {
+        return $"Event: {EventTitle}\nDescription: {Description}\nDate: {Date.ToShortDateString()}\nTime: {Time}\nAddress: {Address.GetFullAddress()}";
+    }
+
+    public virtual string GenerateFullDetailsMessage()
+    {
+        return GenerateStandardMessage();
+    }
+
+    public virtual string GenerateShortDescription()
+    {
+        return $"Type: Event, Title: {EventTitle}, Date: {Date.ToShortDateString()}";
+    }
+}
+
+class Lecture : Event
+{
+    public string Speaker { get; set; }
+    public int Capacity { get; set; }
+
+    public Lecture(string title, string desc, DateTime date, TimeSpan time, Address address, string speaker, int capacity)
+        : base(title, desc, date, time, address)
+    {
+        Speaker = speaker;
+        Capacity = capacity;
+    }
+
+    public override string GenerateFullDetailsMessage()
+    {
+        return base.GenerateFullDetailsMessage() + $"\nSpeaker: {Speaker}\nCapacity: {Capacity}";
+    }
+}
+
+class Reception : Event
+{
+    public string RSVP { get; set; }
+
+    public Reception(string title, string desc, DateTime date, TimeSpan time, Address address, string rsvp)
+        : base(title, desc, date, time, address)
+    {
+        RSVP = rsvp;
+    }
+
+    public override string GenerateFullDetailsMessage()
+    {
+        return base.GenerateFullDetailsMessage() + $"\nRSVP: {RSVP}";
+    }
+}
+
+class OutdoorGathering : Event
+{
+    public string WeatherForecast { get; set; }
+
+    public OutdoorGathering(string title, string desc, DateTime date, TimeSpan time, Address address, string forecast)
+        : base(title, desc, date, time, address)
+    {
+        WeatherForecast = forecast;
+    }
+
+    public override string GenerateFullDetailsMessage()
+    {
+        return base.GenerateFullDetailsMessage() + $"\nWeather Forecast: {WeatherForecast}";
+    }
+}
+
+class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string State { get; set; }
+    public string Country { get; set; }
+
+    public string GetFullAddress()
+    {
+        return $"{Street}, {City}, {State}, {Country}";
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation3 World!");
+        Address address = new Address { Street = "456 Elm St", City = "Othertown", State = "NY", Country = "USA" };
+
+        Event lecture = new Lecture("Lecture Title", "Lecture", DateTime.Now, TimeSpan.FromHours(2), address, "John Doe", 50);
+        Event reception = new Reception("Reception Title", "Reception", DateTime.Now, TimeSpan.FromHours(3), address, "RSVP@mail.com");
+        Event outdoorGathering = new OutdoorGathering("Outdoor Gathering Title", "Outdoor Gathering", DateTime.Now, TimeSpan.FromHours(4), address, "Sunny");
+
+        Console.WriteLine("Standard Message:");
+        Console.WriteLine(lecture.GenerateStandardMessage());
+        Console.WriteLine(reception.GenerateStandardMessage());
+        Console.WriteLine(outdoorGathering.GenerateStandardMessage());
+
+        Console.WriteLine("\nFull Details Message:");
+        Console.WriteLine(lecture.GenerateFullDetailsMessage());
+        Console.WriteLine(reception.GenerateFullDetailsMessage());
+        Console.WriteLine(outdoorGathering.GenerateFullDetailsMessage());
+
+        Console.WriteLine("\nShort Description:");
+        Console.WriteLine(lecture.GenerateShortDescription());
+        Console.WriteLine(reception.GenerateShortDescription());
+        Console.WriteLine(outdoorGathering.GenerateShortDescription());
     }
 }
--- final/Foundation4/Program.cs
+++ final/Foundation4/Program.cs
@@ -1,9 +1,139 @@
 using System;
+using System.Collections.Generic;
+
+class Activity
+{
+    public DateTime Date { get; set; }
+    public int LengthInMinutes { get; set; }
+
+    public Activity(DateTime date, int length)
+    {
+        Date = date;
+        LengthInMinutes = length;
+    }
+
+    public virtual double GetDistance()
+    {
+        return 0;
+    }
+
+    public virtual double GetSpeed()
+    {
+        return 0;
+    }
+
+    public virtual double GetPace()
+    {
+        return 0;
+    }
+
+    public virtual string GetSummary()
+    {
+        return $"{Date.ToShortDateString()} {GetType().Name} ({LengthInMinutes} min)";
+    }
+}
+
+class Running : Activity
+{
+    public double Distance { get; set; }
+
+    public Running(DateTime date, int length, double distance)
+        : base(date, length)
+    {
+        Distance = distance;
+    }
+
+    public override double GetDistance()
+    {
+        return Distance;
+    }
+
+    public override double GetSpeed()
+    {
+        return Distance / (LengthInMinutes / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return LengthInMinutes / Distance;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Distance: {Distance} miles, Speed: {GetSpeed()} mph, Pace: {GetPace()} min/mile";
+    }
+}
+
+class StationaryBicycle : Activity
+{
+    public double Speed { get; set; }
+
+    public StationaryBicycle(DateTime date, int length, double speed)
+        : base(date, length)
+    {
+        Speed = speed;
+    }
+
+    public override double GetSpeed()
+    {
+        return Speed;
+    }
+
+    public override double GetPace()
+    {
+        return 60 / Speed;
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Speed: {Speed} mph, Pace: {GetPace()} min/mile";
+    }
+}
+
+class Swimming : Activity
+{
+    public int Laps { get; set; }
+
+    public Swimming(DateTime date, int length, int laps)
+        : base(date, length)
+    {
+        Laps = laps;
+    }
+
+    public override double GetDistance()
+    {
+        return Laps * 50 / 1000.0;
+    }
+
+    public override double GetSpeed()
+    {
+        return GetDistance() / (LengthInMinutes / 60.0);
+    }
+
+    public override double GetPace()
+    {
+        return LengthInMinutes / GetDistance();
+    }
+
+    public override string GetSummary()
+    {
+        return $"{base.GetSummary()} - Distance: {GetDistance()} km, Speed: {GetSpeed()} kph, Pace: {GetPace()} min/km";
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation4 World!");
+        List<Activity> activities = new List<Activity>();
+
+        activities.Add(new Running(new DateTime(2022, 11, 3), 30, 3.0));
+        activities.Add(new StationaryBicycle(new DateTime(2022, 11, 3), 30, 20));
+        activities.Add(new Swimming(new DateTime(2022, 11, 3), 30, 10));
+
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
